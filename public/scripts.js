e712cdf68ce4b47635d42bbb16bc30ffd96f2dad
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2021-06-18 15:05:37 -0300

    version 0.3

--- public/scripts.js
+++ public/scripts.js
@@ -10,9 +10,21 @@ function save () {
 		email: document.querySelector('input#email').value,
 		age: document.querySelector('input#age').value,
 		funcao: document.querySelector('select#function').value,
-		//melhorias: document.querySelector(''),
+		melhorias: checkbox(),
 		commits: document.querySelector('textarea#comentario').value
 	}
 
+	list_form.push(forms)
 	console.log(forms)
+}
+
+function checkbox() {
+	let checks = document.getElementsByName('prefer')
+	let melhorias = []
+	for(let c = 0; c < checks.length; c++) {
+		if(checks[c].checked == true) {
+			melhorias.push(checks[c].value)
+		}
+	}
+	return melhorias
 }
