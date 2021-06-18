25cb16a2cceb7ef16470189da2c305aec73cb31a
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2021-06-18 11:11:53 -0300

    version 0.2

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,18 @@
+let btn_submit = document.querySelector('input#btn_submit')
+let forms = {}
+let list_form = []
 
+btn_submit.addEventListener('click', save)
+
+function save () {
+	forms = {
+		name: document.querySelector('input#name').value,
+		email: document.querySelector('input#email').value,
+		age: document.querySelector('input#age').value,
+		funcao: document.querySelector('select#function').value,
+		//melhorias: document.querySelector(''),
+		commits: document.querySelector('textarea#comentario').value
+	}
+
+	console.log(forms)
+}
